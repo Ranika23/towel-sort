89dc175e6da5d54bd9617de37f3ba82106cad391
Ranika23 <bveronikal@gmail.com>
2023-08-16 15:25:37 +0300

    docs: add function in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  for (let k in matrix) {
+    k % 2 != 0 ? matrix[k].reverse() : matrix[k]
+    console.log(matrix[k])
+}
+let result = []
+for (let i in matrix) {
+    result.push(...matrix[i])
+}
+  return result;
 }
